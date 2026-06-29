7259222cde645e6cb8179605755b9061f960d910
Orel David <orelmeister@gmail.com>
2026-06-28 21:44:41 -0700

    feat: add LinkedIn Insight Tag (partner 10460409) site-wide in root layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -95,6 +95,15 @@ export default function RootLayout({
           }}
         />
         {/* End Microsoft Clarity */}
+        {/* LinkedIn Insight Tag */}
+        <Script
+          id="linkedin-insight"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `_linkedin_partner_id="10460409";window._linkedin_data_partner_ids=window._linkedin_data_partner_ids||[];window._linkedin_data_partner_ids.push(_linkedin_partner_id);(function(l){if(!l){window.lintrk=function(a,b){window.lintrk.q.push([a,b])};window.lintrk.q=[]}var s=document.getElementsByTagName("script")[0];var b=document.createElement("script");b.type="text/javascript";b.async=true;b.src="https://snap.licdn.com/li.lms-analytics/insight.min.js";s.parentNode.insertBefore(b,s);})(window.lintrk);`,
+          }}
+        />
+        {/* End LinkedIn Insight Tag */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -149,6 +158,17 @@ export default function RootLayout({
           />
         </noscript>
         {/* End Google Tag Manager (noscript) */}
+        {/* LinkedIn Insight Tag (noscript) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=10460409&fmt=gif"
+          />
+        </noscript>
+        {/* End LinkedIn Insight Tag (noscript) */}
         {/* Disable Cloudflare Email Address Obfuscation for entire body */}
         {/* CF replaces emails with <span> elements which breaks React hydration */}
         <div dangerouslySetInnerHTML={{ __html: '<!--email_off-->' }} style={{ display: 'none' }} />
